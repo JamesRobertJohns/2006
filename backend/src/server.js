bb53f78c34989bb4dbbd13b4233d2d8979a5718b
BLTech-py <bryanleetech@gmail.com>
2025-02-05 10:18:13 +0800

    Add OneMap

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/api/db', async (req, res) => {
+    const { name } = req.params
+
+    const uri = 'mongodb://127.0.0.1:27017';
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+
+    const db = client.db('hdb-db');
+});
 
 app.get('/hello', function(req, res) {
     res.send('Get Hello ' + req.body.name);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Map from "react-map-gl/maplibre";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Map
+      style={{ width: '100vw', height: '100vh' }}
+      maxBounds={[103.596, 1.1443, 104.1, 1.4835]}
+      mapStyle="https://www.onemap.gov.sg/maps/json/raster/mbstyle/Grey.json"
+    />
+  </div>
+  );
 }
 
 export default App
