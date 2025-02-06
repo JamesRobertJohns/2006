6bb120f91446f227bd07d69806053cdfdfebf76b
BLTech-py <bryanleetech@gmail.com>
2025-02-06 10:23:58 +0800

    Change map layout from grey to default

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import './App.css'
 import Map from "react-map-gl/maplibre";
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <Map
-      style={{ width: '100vw', height: '100vh' }}
+      style={{ width: '100%', height: '100%' }}
       maxBounds={[103.596, 1.1443, 104.1, 1.4835]}
-      mapStyle="https://www.onemap.gov.sg/maps/json/raster/mbstyle/Grey.json"
+      mapStyle="https://www.onemap.gov.sg/maps/json/raster/mbstyle/Default.json"
     />
   </div>
   );
