a207b51d6fc26d547e3679aa2dd6a9023eec32c6
JamesRobertJohns <jyng1503@gmail.com>
2025-02-12 21:02:54 +0800

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,18 @@ import RegionalMap from './RegionalMap';
 
 function App() {
   return (
-    
-    <div>
+
+    <>
       <progressBar />
       <DropDownMenu />
       <RegionalMap />
       <Map
-      style={{ width: '75vw', height: '75vh' }}
-      maxBounds={[103.596, 1.1443, 104.1, 1.4835]}
-      mapStyle="https://www.onemap.gov.sg/maps/json/raster/mbstyle/Grey.json"
-    />
-  </div>
+        style={{ width: '75vw', height: '75vh' }}
+        maxBounds={[103.596, 1.1443, 104.1, 1.4835]}
+        mapStyle="https://www.onemap.gov.sg/maps/json/raster/mbstyle/Grey.json"
+      />
+    </>
+
   );
 }
 
