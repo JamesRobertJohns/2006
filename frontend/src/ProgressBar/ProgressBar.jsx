0b1ee013e48b3a5462b76fbad083aefa5fbbc2ef
JamesRobertJohns <jyng1503@gmail.com>
2025-02-13 01:32:55 +0800

    i haven't figured out a clean way to implement this, also i am not sure if it is still needed

---
+++ frontend/src/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,13 @@
+import React, { useState,  useContext } from "react";
+
+function ProgressBar() {
+
+   return (
+    <div>
+      Progress Bar Placeholder -- do we still need?
+    </div>
+  );
+
+}
+
+export default ProgressBar
